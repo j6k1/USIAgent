97ca71a62f6516523d51f4d6d43b1d74f5fbdd0c
jinpu <jinpu@LAPTOP-J1SUAON8>
2019-05-01 18:35:05 +0900

    テストケース追加 is_mateのテストケースを追加。後手番はこれから

--- tests/rule.rs
+++ tests/rule.rs
@@ -23434,7 +23434,7 @@ fn test_is_win_gote() {
 	}
 }
 #[test]
-fn test_is_mate_sente() {
+fn test_is_mate_to_sente() {
 	let kinds:Vec<Vec<KomaKind>> = vec![
 		vec![ SFu ],
 		vec![ SKei ],
@@ -23502,10 +23502,98 @@ fn test_is_mate_sente() {
 					}
 				}
 
-				assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed: `(left == right), move = {:?}, {:?}",mv,state.get_banmen());
+				assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
 			}
 		}
 	}
+}
+#[test]
+fn test_is_mate_put_sente() {
+	let kinds:Vec<MochigomaKind> = vec![
+		MochigomaKind::Fu,
+		MochigomaKind::Kyou,
+		MochigomaKind::Kei,
+		MochigomaKind::Gin,
+		MochigomaKind::Kin,
+		MochigomaKind::Kaku,
+		MochigomaKind::Hisha,
+	];
+
+	let mvs:Vec<Vec<(u32,u32)>> = vec![
+		vec![(4,5)],
+		vec![(4,8)],
+		vec![(3,6),(5,6)],
+		vec![(3,3),(3,5),(4,5),(5,3),(5,5)],
+		vec![(3,4),(3,5),(4,3),(4,5),(5,4),(5,5)],
+		vec![(2,2),(6,2),(6,6),(2,6)],
+		vec![(4,1),(1,4),(7,4),(4,7)],
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for (k,mvs) in kinds.iter().zip(&mvs) {
+		for m in mvs {
+			let mut banmen = blank_banmen.clone();
+
+			banmen.0[3][4] = GOu;
+
+			let mut state = State::new(banmen);
+
+			let mut mc = MochigomaCollections::Empty;
+
+			let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,nmc,_) => {
+					state = next;
+					mc = nmc;
+				}
+			}
+
+			let mv = Move::Put(*k,KomaDstPutPosition(9-m.0,m.1+1));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,_,_) => {
+					state = next;
+				}
+			}
+
+			assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+		}
+	}
+}
+#[test]
+fn test_is_mate_with_kyou_sente() {
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	let mut banmen = blank_banmen.clone();
+
+	banmen.0[3][4] = GOu;
+	banmen.0[8][4] = SKyou;
+	banmen.0[5][4] = GKin;
+
+	let mut state = State::new(banmen);
+
+	let mut mc = MochigomaCollections::Empty;
+
+	let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+	match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+		(next,nmc,_) => {
+			state = next;
+			mc = nmc;
+		}
+	}
+
+	let mv = Move::To(KomaSrcPosition(9-4,5+1),KomaDstToPosition(9-3,5+1,false));
+
+	match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+		(next,_,_) => {
+			state = next;
+		}
+	}
+
+	assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
 }
 #[test]
 fn test_is_mate_with_kaku_2step_sente() {
@@ -23567,42 +23655,9 @@ fn test_is_mate_with_kaku_2step_sente() {
 				}
 			}
 
-			assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed: `(left == right), move = {:?}, {:?}",mv,state.get_banmen());
+			assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
 		}
 	}
-}
-#[test]
-fn test_is_mate_with_kyou_sente() {
-	let blank_banmen = Banmen([[Blank; 9]; 9]);
-
-	let mut banmen = blank_banmen.clone();
-
-	banmen.0[3][4] = GOu;
-	banmen.0[8][4] = SKyou;
-	banmen.0[5][4] = GKin;
-
-	let mut state = State::new(banmen);
-
-	let mut mc = MochigomaCollections::Empty;
-
-	let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
-
-	match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
-		(next,nmc,_) => {
-			state = next;
-			mc = nmc;
-		}
-	}
-
-	let mv = Move::To(KomaSrcPosition(9-4,5+1),KomaDstToPosition(9-3,5+1,false));
-
-	match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
-		(next,_,_) => {
-			state = next;
-		}
-	}
-
-	assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed: `(left == right), move = {:?}, {:?}",mv,state.get_banmen());
 }
 #[test]
 fn test_is_mate_with_hisha_2step_sente() {
@@ -23664,7 +23719,390 @@ fn test_is_mate_with_hisha_2step_sente() {
 				}
 			}
 
-			assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed: `(left == right), move = {:?}, {:?}",mv,state.get_banmen());
+			assert!(Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+		}
+	}
+}
+#[test]
+fn test_is_mate_no_mate_to_sente() {
+	let kinds:Vec<Vec<KomaKind>> = vec![
+		vec![ SFu ],
+		vec![ SKei ],
+		vec![ SGin ],
+		vec![ SKin, SFuN, SKyouN, SKeiN, SGinN ],
+		vec![ SKaku ],
+		vec![ SHisha ],
+		vec![ SKakuN ],
+		vec![ SHishaN ],
+		vec![ SOu ]
+	];
+
+	let positions:Vec<Vec<(u32,u32)>> = vec![
+		vec![(4,7)],
+		vec![(3,8),(5,8)],
+		vec![(2,3),(3,2),(5,2),(6,3)],
+		vec![(3,2),(5,2)],
+		vec![(2,2),(6,2),(6,6),(2,6)],
+		vec![(4,1),(1,4),(7,4),(4,7)],
+		vec![(2,2),(6,2),(6,6),(2,6),(3,4),(4,3),(4,5),(5,4)],
+		vec![(4,1),(1,4),(7,4),(4,7),(3,3),(3,5),(5,3),(5,5)],
+		vec![(1,1),(1,4),(1,7),(4,1),(4,7),(7,1),(7,4),(7,7)]
+	];
+
+	let mvs:Vec<Vec<(u32,u32)>> = vec![
+		vec![(4,6)],
+		vec![(4,6)],
+		vec![(3,4),(4,3),(4,3),(5,4)],
+		vec![(3,3),(5,3)],
+		vec![(0,4),(4,0),(4,8),(8,4)],
+		vec![(1,1),(1,7),(7,1),(7,7)],
+		vec![(0,4),(4,0),(4,8),(8,4),(2,4),(4,2),(4,6),(6,4)],
+		vec![(1,1),(1,7),(7,1),(7,7),(2,2),(2,6),(6,2),(6,6)],
+		vec![(2,2),(2,4),(2,6),(4,2),(4,6),(6,2),(6,4),(6,6)],
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for ((p,kinds),mvs) in positions.iter().zip(&kinds).zip(&mvs) {
+		for (k,p) in kinds.iter().zip(p) {
+			for m in mvs {
+				let mut banmen = blank_banmen.clone();
+
+				banmen.0[3][4] = GOu;
+				banmen.0[p.1 as usize][p.0 as usize] = *k;
+
+				let mut state = State::new(banmen);
+
+				let mut mc = MochigomaCollections::Empty;
+
+				let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+				match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+					(next,nmc,_) => {
+						state = next;
+						mc = nmc;
+					}
+				}
+
+				let mv = Move::To(KomaSrcPosition(9-p.0,p.1+1),KomaDstToPosition(9-m.0,m.1+1,false));
+
+				match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+					(next,_,_) => {
+						state = next;
+					}
+				}
+
+				assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+			}
+		}
+	}
+}
+#[test]
+fn test_is_mate_no_mate_put_sente() {
+	let kinds:Vec<MochigomaKind> = vec![
+		MochigomaKind::Fu,
+		MochigomaKind::Kei,
+		MochigomaKind::Gin,
+		MochigomaKind::Kin,
+		MochigomaKind::Kaku,
+		MochigomaKind::Hisha,
+	];
+
+	let mvs:Vec<Vec<(u32,u32)>> = vec![
+		vec![(4,6)],
+		vec![(4,6)],
+		vec![(3,4),(4,3),(4,3),(5,4)],
+		vec![(3,3),(5,3)],
+		vec![(0,4),(4,0),(4,8),(8,4)],
+		vec![(1,1),(1,7),(7,1),(7,7)],
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for (k,mvs) in kinds.iter().zip(&mvs) {
+		for m in mvs {
+			let mut banmen = blank_banmen.clone();
+
+			banmen.0[3][4] = GOu;
+
+			let mut state = State::new(banmen);
+
+			let mut mc = MochigomaCollections::Empty;
+
+			let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,nmc,_) => {
+					state = next;
+					mc = nmc;
+				}
+			}
+
+			let mv = Move::Put(*k,KomaDstPutPosition(9-m.0,m.1+1));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,_,_) => {
+					state = next;
+				}
+			}
+
+			assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+		}
+	}
+}
+#[test]
+fn test_is_mate_no_mate_with_kyou_occupied_self_sente() {
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	let mut banmen = blank_banmen.clone();
+
+	banmen.0[3][4] = GOu;
+	banmen.0[8][4] = SKyou;
+	banmen.0[6][4] = SKin;
+
+	let mut state = State::new(banmen);
+
+	let mc = MochigomaCollections::Empty;
+
+	let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+	match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+		(next,_,_) => {
+			state = next;
+		}
+	}
+
+	assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+}
+#[test]
+fn test_is_mate_no_mate_with_kaku_occupied_self_sente() {
+	let kinds:Vec<KomaKind> = vec![ SKaku, SKakuN ];
+
+	let positions:Vec<(u32,u32)> = vec![
+		(0,4),(4,0),(4,8),(8,4)
+	];
+
+	let mvs:Vec<(u32,u32)> = vec![
+		(2,2),(6,2),(6,6),(2,6)
+	];
+
+	let occ_positions:Vec<(u32,u32)> = vec![
+		(3,3),(5,3),(5,5),(3,5)
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for k in &kinds {
+		for ((p,m),op) in positions.iter().zip(&mvs).zip(&occ_positions) {
+			let mut banmen = blank_banmen.clone();
+
+			banmen.0[3][4] = GOu;
+			banmen.0[p.1 as usize][p.0 as usize] = *k;
+
+			banmen.0[op.1 as usize][op.0 as usize] = SFu;
+
+			let mut state = State::new(banmen);
+
+			let mut mc = MochigomaCollections::Empty;
+
+			let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,nmc,_) => {
+					state = next;
+					mc = nmc;
+				}
+			}
+
+			let mv = Move::To(KomaSrcPosition(9-p.0,p.1+1),KomaDstToPosition(9-m.0,m.1+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,_,_) => {
+					state = next;
+				}
+			}
+
+			assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+		}
+	}
+}
+#[test]
+fn test_is_mate_no_mate_with_hisha_occupied_self_sente() {
+	let kinds:Vec<KomaKind> = vec![ SHisha, SHishaN ];
+
+	let positions:Vec<(u32,u32)> = vec![
+		(1,1),(1,7),(7,1),(7,7),
+	];
+
+	let mvs:Vec<(u32,u32)> = vec![
+		(4,1),(1,4),(7,4),(4,7),
+	];
+
+	let occ_positions:Vec<(u32,u32)> = vec![
+		(4,2),(2,4),(6,4),(4,6)
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for k in &kinds {
+		for ((p,m),op) in positions.iter().zip(&mvs).zip(&occ_positions) {
+			let mut banmen = blank_banmen.clone();
+
+			banmen.0[3][4] = GOu;
+			banmen.0[p.1 as usize][p.0 as usize] = *k;
+
+			banmen.0[op.1 as usize][op.0 as usize] = SKin;
+
+			let mut state = State::new(banmen);
+
+			let mut mc = MochigomaCollections::Empty;
+
+			let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,nmc,_) => {
+					state = next;
+					mc = nmc;
+				}
+			}
+
+			let mv = Move::To(KomaSrcPosition(9-p.0,p.1+1),KomaDstToPosition(9-m.0,m.1+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,_,_) => {
+					state = next;
+				}
+			}
+
+			assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+		}
+	}
+}
+#[test]
+fn test_is_mate_no_mate_with_kyou_occupied_opponent_sente() {
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	let mut banmen = blank_banmen.clone();
+
+	banmen.0[3][4] = GOu;
+	banmen.0[8][4] = SKyou;
+	banmen.0[6][4] = GKin;
+
+	let mut state = State::new(banmen);
+
+	let mc = MochigomaCollections::Empty;
+
+	let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+	match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+		(next,_,_) => {
+			state = next;
+		}
+	}
+
+	assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+}
+#[test]
+fn test_is_mate_no_mate_with_kaku_occupied_opponent_sente() {
+	let kinds:Vec<KomaKind> = vec![ SKaku, SKakuN ];
+
+	let positions:Vec<(u32,u32)> = vec![
+		(0,4),(4,0),(4,8),(8,4)
+	];
+
+	let mvs:Vec<(u32,u32)> = vec![
+		(2,2),(6,2),(6,6),(2,6)
+	];
+
+	let occ_positions:Vec<(u32,u32)> = vec![
+		(3,3),(5,3),(5,5),(3,5)
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for k in &kinds {
+		for ((p,m),op) in positions.iter().zip(&mvs).zip(&occ_positions) {
+			let mut banmen = blank_banmen.clone();
+
+			banmen.0[3][4] = GOu;
+			banmen.0[p.1 as usize][p.0 as usize] = *k;
+
+			banmen.0[op.1 as usize][op.0 as usize] = GFu;
+
+			let mut state = State::new(banmen);
+
+			let mut mc = MochigomaCollections::Empty;
+
+			let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,nmc,_) => {
+					state = next;
+					mc = nmc;
+				}
+			}
+
+			let mv = Move::To(KomaSrcPosition(9-p.0,p.1+1),KomaDstToPosition(9-m.0,m.1+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,_,_) => {
+					state = next;
+				}
+			}
+
+			assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
+		}
+	}
+}
+#[test]
+fn test_is_mate_no_mate_with_hisha_occupied_opponent_sente() {
+	let kinds:Vec<KomaKind> = vec![ SHisha, SHishaN ];
+
+	let positions:Vec<(u32,u32)> = vec![
+		(1,1),(1,7),(7,1),(7,7),
+	];
+
+	let mvs:Vec<(u32,u32)> = vec![
+		(4,1),(1,4),(7,4),(4,7),
+	];
+
+	let occ_positions:Vec<(u32,u32)> = vec![
+		(4,2),(2,4),(6,4),(4,6)
+	];
+
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	for k in &kinds {
+		for ((p,m),op) in positions.iter().zip(&mvs).zip(&occ_positions) {
+			let mut banmen = blank_banmen.clone();
+
+			banmen.0[3][4] = GOu;
+			banmen.0[p.1 as usize][p.0 as usize] = *k;
+
+			banmen.0[op.1 as usize][op.0 as usize] = GKin;
+
+			let mut state = State::new(banmen);
+
+			let mut mc = MochigomaCollections::Empty;
+
+			let mv = Move::To(KomaSrcPosition(9-4,3+1),KomaDstToPosition(9-4,4+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,nmc,_) => {
+					state = next;
+					mc = nmc;
+				}
+			}
+
+			let mv = Move::To(KomaSrcPosition(9-p.0,p.1+1),KomaDstToPosition(9-m.0,m.1+1,false));
+
+			match Rule::apply_move_none_check(&state,Teban::Sente,&mc,mv.to_applied_move()) {
+				(next,_,_) => {
+					state = next;
+				}
+			}
+
+			assert!(!Rule::is_mate(Teban::Sente,&state),"assertion failed, move = {:?}, {:?}",mv,state.get_banmen());
 		}
 	}
 }
