9f243e22f6aa5918fc58bf92d1afa46166bd28b7
jinpu <jinpu@LAPTOP-J1SUAON8>
2019-05-01 11:49:27 +0900

    テストケース追加 後手番のis_put_fu_and_mateのテストケースを追加

--- tests/rule.rs
+++ tests/rule.rs
@@ -23312,6 +23312,59 @@ fn test_is_put_fu_and_mate_sente() {
 								"assertion failed: `(left == right), move = {:?}, {:?}",m,state.get_banmen())
 	}
 }
+#[test]
+fn test_is_put_fu_and_mate_gote() {
+	let blank_banmen = Banmen([[Blank; 9]; 9]);
+
+	let m = Move::Put(MochigomaKind::Fu,KomaDstPutPosition(9-(8-4),(8-1)+1));
+
+	let position_and_kinds:Vec<Vec<(usize,usize,KomaKind)>> = vec![
+		vec![
+			(8-3,8-0,SFu),(8-3,8-1,SKyou),(8-4,8-0,SOu),(8-5,8-0,SFu),(8-5,8-1,SKyou),(8-4,8-8,GKyou)
+		],
+		vec![
+			(8-3,8-0,SKin),(8-3,8-1,SKyou),(8-4,8-0,SOu),(8-5,8-0,SKin),(8-5,8-1,SKyou),(8-4,8-8,GKyou)
+		],
+		vec![
+			(8-3,8-0,SFu),(8-3,8-1,SKyou),(8-4,8-0,SOu),(8-5,8-1,SKyou),(8-4,8-8,GKyou)
+		],
+		vec![
+			(8-3,8-0,SFu),(8-3,8-1,SKyou),(8-4,8-0,SOu),(8-5,8-0,SKyou),(8-5,8-1,SGin)
+		]
+	];
+
+	let answer:[bool; 4] = [
+		true,false,false,false
+	];
+
+	for (pk,answer) in position_and_kinds.iter().zip(&answer) {
+		let mut banmen = blank_banmen.clone();
+
+		for pk in pk {
+			banmen.0[pk.1][pk.0] = pk.2;
+		}
+
+		let mut state = State::new(banmen);
+
+		let mut mg:HashMap<MochigomaKind,u32> = HashMap::new();
+		let ms:HashMap<MochigomaKind,u32> = HashMap::new();
+
+		mg.insert(MochigomaKind::Fu,1);
+
+		let mut mc = MochigomaCollections::Pair(ms,mg);
+
+		match Rule::apply_move_none_check(&state,Teban::Gote,&mc,m.to_applied_move()) {
+			(next,nmc,_) => {
+				state = next;
+				mc = nmc;
+			}
+		}
+
+		assert_eq!(*answer,Rule::is_put_fu_and_mate(
+								&state,Teban::Gote,&mc,m.to_applied_move()),
+								"assertion failed: `(left == right), move = {:?}, {:?}",m,state.get_banmen())
+	}
+}
 impl From<rule::LegalMove> for LegalMove {
 	fn from(m:rule::LegalMove) -> LegalMove {
 		match m {
