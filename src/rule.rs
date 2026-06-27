f3679f6c3ec630fbc9f17ba21c1c2edfc36bc6a6
j6k1 <nanasi18@gmail.com>
2026-06-27 12:07:50 +0900

    バグ修正 pin駒でない駒のunpinningの効きの計算に誤りがあったので修正した

--- src/rule.rs
+++ src/rule.rs
@@ -16094,7 +16094,9 @@ impl Rule {
 		flip_self_occupied_board:BitBoard,
 		flip_opponent_occupied_board:BitBoard,
 	) -> BitBoard {
-		if let Some(p) = ou_position_board.iter().next() {
+		if pin_board & (1 << (from + 1)) != 0 {
+			return BitBoard::default();
+		} else if let Some(p) = ou_position_board.iter().next() {
 			let p = p as u32;
 
 			let from_mask = BitBoard::from(1 << (from + 1));
