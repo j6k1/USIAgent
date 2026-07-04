ad8f0aedcd9f31daaff2e488379d3d238b8c604f
j6k1 <nanasi18@gmail.com>
2026-07-04 16:16:35 +0900

    仕様変更、リファクタリング、テストケース追加 一部定数を公開定数とした gen_ou_surrounding_maskの範囲外の値のマスク処理を一部変更した gen_control_bits_by_kyou,gen_control_bits_by_kaku,gen_control_bits_by_hishaのテストケースを追加した

--- tests/rule/gen_mask.rs
+++ tests/rule/gen_mask.rs
@@ -1,5 +1,5 @@
 use usiagent::bitboard::BitBoard;
-use usiagent::rule::State;
+use usiagent::rule::{State, H_MASK, KAKU_TO_RIGHT_BOTTOM_MASK_MAP, KAKU_TO_RIGHT_TOP_MASK_MAP, V_MASK};
 use usiagent::shogi::{Banmen, KomaKind, Teban};
 use usiagent::shogi::KomaKind::Blank;
 #[allow(dead_code)]
@@ -40,5 +40,96 @@ fn gen_ou_surrounding_mask_test()
 
         assert_eq!(usiagent::rule::Rule::gen_ou_surrounding_mask(t,state.get_part()),expected << 1,"index = {}",i);
         assert_eq!(usiagent::rule::Rule::gen_ou_surrounding_mask(t.opposite(),state.get_part()),BitBoard::default(),"index = {}",i);
+    }
+}
+#[test]
+fn test_gen_control_bits_by_kyou() {
+    let test_data = vec![
+        (vec![((4,6,KomaKind::SKyou))],4,6,BitBoard::from(0b000111111u128 << 36))
+    ];
+
+    for (i,(pieces,sx,sy,expected)) in test_data.into_iter().enumerate() {
+        let mut b = blank();
+        for &(x,y,k) in pieces.iter() {
+            set_piece(&mut b,x,y,k);
+        }
+        let state = State::new(b);
+
+        assert_eq!(usiagent::rule::Rule::gen_control_bits_by_kyou(
+            state.get_part().gote_opponent_board,
+            state.get_part().gote_self_board,
+            idx(sx,sy)
+        ).reverse(),
+        expected << 1,"index = {}",i);
+    }
+}
+#[test]
+fn test_gen_control_bits_by_kaku() {
+    let test_data = vec![
+        (vec![((3,3,KomaKind::SKaku))],3,3,
+         (BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[idx(3,3) as usize]) |
+          BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[idx(3,3) as usize])) & !(1 << idx(3,3))
+        ),
+        (vec![((3,5,KomaKind::SKaku))],3,5,
+         (BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[idx(3,5) as usize]) |
+             BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[idx(3,5) as usize])) & !(1 << idx(3,5))
+        ),
+        (vec![((5,3,KomaKind::SKaku))],5,3,
+         (BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[idx(5,3) as usize]) |
+             BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[idx(5,3) as usize])) & !(1 << idx(5,3))
+        ),
+        (vec![((5,5,KomaKind::SKaku))],5,5,
+         (BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[idx(5,5) as usize]) |
+             BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[idx(5,5) as usize])) & !(1 << idx(5,5))
+        ),
+    ];
+
+    for (i,(pieces,sx,sy,expected)) in test_data.into_iter().enumerate() {
+        let mut b = blank();
+        for &(x,y,k) in pieces.iter() {
+            set_piece(&mut b,x,y,k);
+        }
+        let state = State::new(b);
+
+        assert_eq!(usiagent::rule::Rule::gen_control_bits_by_kaku(
+            state.get_part().sente_self_board,
+            state.get_part().sente_opponent_board,
+            state.get_part().gote_opponent_board,
+            state.get_part().gote_self_board,
+            idx(sx,sy)
+        ), expected << 1,"index = {}",i);
+    }
+}
+#[test]
+fn test_gen_control_bits_by_hisha() {
+    let test_data = vec![
+        (vec![((3,3,KomaKind::SHisha))],3,3,(
+            BitBoard::from(V_MASK << (3 * 9)) | BitBoard::from(H_MASK << 3)
+        ) & !(1 << idx(3,3))),
+        (vec![((3,5,KomaKind::SHisha))],3,5,(
+            BitBoard::from(V_MASK << (3 * 9)) | BitBoard::from(H_MASK << 5)
+        ) & !(1 << idx(3,5))),
+        (vec![((5,3,KomaKind::SHisha))],5,3,(
+            BitBoard::from(V_MASK << (5 * 9)) | BitBoard::from(H_MASK << 3)
+        ) & !(1 << idx(5,3))),
+        (vec![((5,5,KomaKind::SHisha))],5,5,(
+            BitBoard::from(V_MASK << (5 * 9)) | BitBoard::from(H_MASK << 5)
+        ) & !(1 << idx(5,5)))
+    ];
+
+    for (i,(pieces,sx,sy,expected)) in test_data.into_iter().enumerate() {
+        let mut b = blank();
+        for &(x,y,k) in pieces.iter() {
+            set_piece(&mut b,x,y,k);
+        }
+        let state = State::new(b);
+
+        assert_eq!(usiagent::rule::Rule::gen_control_bits_by_hisha(
+            state.get_part().sente_self_board,
+            state.get_part().sente_opponent_board,
+            state.get_part().gote_opponent_board,
+            state.get_part().gote_self_board,
+            idx(sx,sy)
+        ), expected << 1,"index = {}",i);
     }
 }
