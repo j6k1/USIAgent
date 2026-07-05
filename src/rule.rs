080386b56fb9db65ecd72afa315247ffbdf78f7b
j6k1 <nanasi18@gmail.com>
2026-07-05 10:47:52 +0900

    機能追加 Blocker判定に使うマスクの生成メソッドを実装した。

--- src/rule.rs
+++ src/rule.rs
@@ -16765,6 +16765,107 @@ impl Rule {
 		}
 	}
 
+	/// 飛車、角、香車の王手ラインを遮断する可能性のある駒をフィルタするマスクを生成する。マスクは常に先手視点で生成される。
+	///
+	/// # Arguments
+	///
+	/// * `taban` - 王手をかけている方の手番
+	/// * `state` - 盤面の状態
+	/// * `from` - 王手をかけている駒の位置。常に先手座標で指定する。
+	/// 渡した引数の状態が不正な場合の動作は未定義
+	#[inline]
+	pub fn gen_mask_for_possible_ou_blocker_candidate(teban:Teban,state:&State,from:u32) -> BitBoard {
+		let op = Rule::ou_square(teban,state);
+
+		if op == -1 {
+			return BitBoard::default();
+		}
+
+		let (ox,oy) = op.square_to_point();
+		let (sx,sy) = from.square_to_point();
+
+		if sx != ox && sy != oy && (ox as i32 - sx as i32).abs() != (oy as i32 - sy as i32).abs() {
+			return BitBoard::default();
+		}
+
+		if ox == sx && ox == 0 {
+			BitBoard::from(V_MASK << 9)
+		} else if ox == sx && ox == 8 {
+			BitBoard::from(V_MASK >> (8 * 9 - 9))
+		} else if ox == sx {
+			BitBoard::from(V_MASK >> (ox * 9 - 9)) | BitBoard::from(V_MASK << (oy * 9 + 9))
+		} else if oy == sy && oy == 0 {
+			BitBoard::from(H_MASK << 1)
+		} else if oy == sy && oy == 8 {
+			BitBoard::from(H_MASK >> 7)
+		} else if oy == sy {
+			BitBoard::from(H_MASK >> (oy - 1)) | BitBoard::from(H_MASK << (oy + 1))
+		} else if ox < sx && oy < sy {
+			BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize + 1]) |
+			BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize + 9])
+		} else if ox > sx && oy > sy {
+			BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize - 1]) |
+			BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize - 9])
+		} else if ox < sx {
+			BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[op as usize - 1]) |
+			BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[op as usize + 9])
+		} else {
+			BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize + 1]) |
+			BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize - 9])
+		}
+	}
+
+	/// 飛車、角、香車の王手ラインを確実に遮断できる位置にいる歩の位置を抽出するビットボードを生成する。常に先手視点のビットボードを返す。
+	///
+	/// # Arguments
+	///
+	/// * `taban` - 王手をかけている方の手番
+	/// * `state` - 盤面の状態
+	/// * `from` - 王手をかけている駒の位置。常に先手座標で指定する。
+	/// 渡した引数の状態が不正な場合の動作は未定義
+	#[inline]
+	pub fn gen_mask_for_possible_ou_blocker_by_fu(teban:Teban,state:&State,from:u32) -> BitBoard {
+		let op = Rule::ou_square(teban,state);
+
+		if op == -1 {
+			return BitBoard::default();
+		}
+
+		let (ox,oy) = op.square_to_point();
+		let (sx,sy) = from.square_to_point();
+
+		let dx = (ox as i32 - sx as i32).abs();
+		let dy = (oy as i32 - sy as i32).abs();
+
+		if sx != ox && sy != oy && (dx != dy || dx <= 1 || dy <= 1) {
+			return BitBoard::default();
+		}
+
+		if ox == sx {
+			BitBoard::default()
+		} else if oy == sy && oy < 8 && ox + 1 < sx {
+			let b = BitBoard::from(1 << (from + 1 - 9));
+			((b - (1 << (op + 1 + 9))) ^ b) & (V_MASK << (oy + 1))
+		} else if oy == sy && oy < 8 && ox - 1 > sx {
+			let b = BitBoard::from(1 << (op + 1 - 9));
+			((b - (1 << (from + 1 + 9))) ^ b) & (V_MASK >> (oy + 1))
+		} else if ox < sx && oy < sy {
+			let b = BitBoard::from(1 << (from - 9));
+			((b - (1 << (op + 9 + 2))) ^ b) & BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[from as usize - 9])
+		} else if ox > sx && oy > sy {
+			let b = BitBoard::from(1 << (op - 9));
+			((b - (1 << (from + 9 + 2))) ^ b) & BitBoard::from(KAKU_TO_RIGHT_BOTTOM_MASK_MAP[op as usize - 9])
+		} else if ox < sx && oy > sy {
+			let b = BitBoard::from(1 << (from - 9 + 2));
+			((b - (1 << (op + 9))) ^ b) & BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[from as usize + 1])
+		} else if ox > sx && oy < sy {
+			let b = BitBoard::from(1 << (op - 9 + 2));
+			((b - (1 << (from + 9))) ^ b) & BitBoard::from(KAKU_TO_RIGHT_TOP_MASK_MAP[op as usize + 1])
+		} else {
+			BitBoard::default()
+		}
+	}
+
 	/// 先手の特定のエリアへの驚異の数を返す
 	///
 	/// # Arguments
